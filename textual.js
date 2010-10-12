688bf3a026a06492e573882588bbf1d5f809a548
Patrick Hall <pat@amundo.com>
2010-10-11 22:20:12 -0700

    rewrite

--- textual.js
+++ textual.js
@@ -1,6 +1,8 @@
 /*
 
   textual.js - a standalone library for various string-processing utilities
+  
+  should also work with nodejs. 
 
   © 2010 Patrick Hall
   pathall@gmail.com
@@ -8,25 +10,77 @@
   MIT License
 
 */
+var text = {
 
-function tokenize(text){
-  return text;
-}
+    trim : function(text){
+      var rtrim = /^(\s|\u00A0)+|(\s|\u00A0)+$/g;
+      return (text || "").replace( rtrim, "" );
+    },
+    
+    extractLines : function(text){
+      return trim(text).split(/\n+/);
+    },
+    
+    depunctuate : function(text){
+      return text.replace(/[\.\?\!]/g, ' ');
+    },
 
-function frequency(sequence){
-  // tally elements of sequence 
-  var tally = {};
-  for(var i = 0 ; i<sequence.length; i++){
-    var element = sequence[i];
-    if (!(element in tally) ){
-      tally[element] = 0;
+    letters : function(text){
+      return text.split('');
+    },
+
+    frequency : function(sequence){
+      var count = {};
+      for (var i=0; i<sequence.length; i++){
+        var elem = sequence[i];
+        if ( elem in count ) {
+          count[elem] += 1;
+        } else {
+          count[elem] = 1;
+        }
+      }
+      return count;
+    },
+
+    bag : function(sequence){
+      var seen = []; 
+      for (var elem in sequence){
+        if (!sequence.hasOwnProperty(elem))
+          continue 
+        seen.push(elem) 
+      }
+      return seen; 
+    },
+
+    charset : function(text){
+      var cs = this.bag( this.frequency( this.letters(text)));
+      cs.sort();
+      return cs;
+    },
+
+    alphabetize : function(sequence, alphabet){
+      // alphabet contains the collation order 
+      sequence.sort(function(a,b){
+        return alphabet.indexOf(a) < alphabet.indexOf(b) ? -1 : 1;
+      });
+      return sequence;
+    },
+
+    tokenize : function(text){
+      return this.trim(this.depunctuate(text)).toLowerCase().split(/[ ]+/);
+    },
+
+    zip : function(a, b){
+      // like python zip
+      result = [];
+      for(var i=0; i<a.length;i++){
+        result.push( [a[i], b[i]] );
+      }
+      return result;
     }
-    tally[element] += 1;
-  } 
-  return tally;
 }
 
-
-function depunctuate(text){
-  return text;
+if(typeof(exports) !== 'undefined' && exports !== null) {
+  exports.textual = textual; 
 }
+    
